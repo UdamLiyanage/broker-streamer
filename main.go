fdafc1c199bfe8e0fdb7e4b682a7bc50867dc359
UdamLiyanage <udam1998@gmail.com>
2020-04-06 09:31:39 +0530

    MQTT Connection Logic

--- main.go
+++ main.go
@@ -1,9 +1,43 @@
 package main
 
 import (
+	"fmt"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	_ "github.com/joho/godotenv/autoload"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
+func onMessageReceived(_ mqtt.Client, msg mqtt.Message) {
+	println(string(msg.Payload()))
+}
+
 func main() {
-	println("Main Function")
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	opts := setClientOptions()
+	topic := "/#"
+
+	opts.OnConnect = func(c mqtt.Client) {
+		if token := c.Subscribe(topic, 0, onMessageReceived); token.Wait() && token.Error() != nil {
+			panic(token.Error())
+		}
+	}
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	} else {
+		fmt.Printf("Connected to server:\n")
+	}
+	<-c
+}
+
+func setClientOptions() *mqtt.ClientOptions {
+	opts := mqtt.NewClientOptions().AddBroker(os.Getenv("BROKER_URL"))
+	opts.SetUsername(os.Getenv("BROKER_USERNAME"))
+	opts.SetPassword(os.Getenv("BROKER_PASSWORD"))
+	opts.SetDefaultPublishHandler(onMessageReceived)
+	return opts
 }
